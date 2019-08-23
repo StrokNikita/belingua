511513310c6a7d05d76dd5779a25f33db8057381
StrokNikita <54074535+StrokNikita@users.noreply.github.com>
2019-08-23 13:49:42 +0300

    Update SliderAdapter.java

--- SliderAdapter.java
+++ SliderAdapter.java
@@ -1,7 +1,6 @@
 package com.example.belingua;
 
 import android.content.Context;
-import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +8,6 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.PagerAdapter;
 
 public class SliderAdapter extends PagerAdapter {
